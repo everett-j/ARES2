47a183ae43f0ba7b22f2292a5483aceb33851234
Josh <jseverett16@gmail.com>
2019-10-22 09:41:42 -0700

    added deploy files

--- ARES/settings.py
+++ ARES/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&n+xsd*f6t2sgserlv1b9l@5t8q5l1)gs%)2^s3xu3)poqewlo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.132.176.90', 'josheverett.info', 'www.josheverett.info']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")	# add this line at the bottom; don't replace any existing lines!
